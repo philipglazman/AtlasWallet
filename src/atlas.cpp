9ca3f6d9ca4c6284c8f695a6a29abb643ce1051d
mango <8378656+MangoSalad@users.noreply.github.com>
2018-01-05 18:28:21 -0500

    first commit

---
+++ src/atlas.cpp
@@ -0,0 +1,48 @@
+/*
+* Main program for Atlas.
+*/
+
+#include "stdafx.h"
+
+/**/
+/*
+int main(int argc, char * argv[])
+
+NAME
+
+        main(int argc, char * argv[]) - Main entry point for program
+
+SYNOPSIS
+
+        n/a
+
+DESCRIPTION
+
+        n/a
+
+RETURNS
+
+        n/a
+
+AUTHOR
+
+        Philip Glazman
+
+DATE
+
+        12/31/2017
+
+*/
+/**/
+int
+main(int argc, char * argv[])
+{
+    // load config file, create new one if not available
+    // If new config file create, suggest to user that they either check config.
+    // if first time loading wallet, go through steps to creating HD keys
+    // asynchronously check for utxo matching addresses
+    //
+}
+
+
+
---
+++ src/stdafx.h
@@ -0,0 +1,17 @@
+// stdafx.h : include file for standard system include files
+
+/*
+TO
+compile as g++ -c stdafx.h -o stdafx.h.gch
+g++ -c stdafx.h -o stdafx.h.gch -std=c++11 -lboostsystem -lbitcoin
+*/
+
+#pragma once
+
+// TODO: add additional headers to the program
+// Include files
+#include <bitcoin/bitcoin.hpp>
+#include <bitcoin/client.hpp>
+#include <string.h>
+#include <iostream>
+#include <cstdint>

5f1df6c9ff73318649a8cb5c79af9cf713f1ea8c
mango <8378656+MangoSalad@users.noreply.github.com>
2018-03-02 18:45:02 -0500

    Added script class

--- src/script.h
+++ src/script.h
@@ -16,7 +16,17 @@ class Script
     
     protected:
 
+
     private:
+        //include stack
+        template <typename T> 
+        std::stack <T> stack;
+
+        //instructions
+        //stack rules
+        //op codes
+
+        std::vector <std::string> opCodes = {"op_0","op_false","op_pushdata1","op_pushdata2","op_pushdata4","op_1negate"}; 
 
 
 };
